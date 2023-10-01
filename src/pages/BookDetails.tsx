1a7b97591892a47b0dd9a092bdc3efc2277a422e
DIDAR HOSSAIN <didarhossain018@gmail.com>
2023-10-02 01:39:24 +0600

    updated book details page

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -1,34 +1,37 @@
 import { Button } from '../components/ui/button';
 import { useParams } from 'react-router-dom';
-import BookReview from '../components/BookReview';
 import { useGetSingleBookQuery } from '../redux/features/books/bookApi';
-import { CloudCog } from 'lucide-react';
+import BookReview from '../components/BookReview';
 
 
 export default function BookDetails() {
   const { id } = useParams();
 
-  const {data:book, isLoading, error} = useGetSingleBookQuery(id)
+  const {data:book, isLoading, error} = useGetSingleBookQuery(id);
   console.log("from book details",book)
 
   return (
     <>
-      <div className="flex max-w-7xl mx-auto items-center border-b border-gray-300">
-        <div className="w-[50%]">
+      <div className="flex h-screen items-center justify-center mx-auto border-b border-gray-300">
+        <div className="w-[50%] flex justify-center">
           <img src={book?.image} alt="" />
         </div>
         <div className="w-[50%] space-y-3">
-          <h1 className="text-3xl font-semibold">{book?.title}</h1>
+          <h1 className="text-3xl font-semibold">Book Title: {book?.title}</h1>
+          <h1 className="text-3xl font-semibold">Author: {book?.author}</h1>
+          <h1 className="text-3xl font-semibold">Price: {book?.price}</h1>
+          <h1 className="text-3xl font-semibold">Genre: {book?.genre}</h1>
+          <h1 className="text-3xl font-semibold">Pubilication Date: {book?.publication_date}</h1>
           <p className="text-xl">Rating: {book?.rating}</p>
           <ul className="space-y-1 text-lg">
-            {book?.features?.map((feature:string) => (
-              <li key={feature}>{feature}</li>
-            ))}
+            {/* {book?.comments?.map((comment:string) => (
+              <li key={comment}>{comment}</li>
+              ))} */}
           </ul>
           <Button>Add to cart</Button>
         </div>
       </div>
-      <BookReview id={id} />
+      <BookReview id={id!} />
     </>
   );
 }
