b2e6cd8e4a4a6b04e54a10d485072f6ea5384343
DIDAR HOSSAIN <didarhossain018@gmail.com>
2023-10-05 00:00:05 +0600

    updated book card

--- src/pages/Books.tsx
+++ src/pages/Books.tsx
@@ -1,13 +1,7 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Label } from '../components/ui/label';
-import { Slider } from '../components/ui/slider';
-import { Switch } from '../components/ui/switch';
 import { useToast } from '../components/ui/use-toast';
 import { useAppDispatch, useAppSelector } from '../redux/hook/hook';
 import { IBook } from '../types/globalTypes';
-import {setPriceRange, toggleState} from '../redux/features/books/bookSlice'
+import {setPriceRange} from '../redux/features/books/bookSlice'
 import BookCard from '../components/BookCard';
 import { useGetBooksQuery } from '../redux/features/books/bookApi';
 
@@ -39,30 +33,8 @@ export default function Books() {
   }
 
   return (
-    <div className="grid grid-cols-12 max-w-7xl mx-auto relative ">
-      <div className="col-span-3 z mr-10 space-y-5 border rounded-2xl border-gray-200/80 p-5 self-start sticky top-16 h-[calc(100vh-80px)]">
-        <div>
-          <h1 className="text-2xl uppercase">Availability</h1>
-          <div onClick={()=>dispatch(toggleState())} className="flex items-center space-x-2 mt-3">
-            <Switch id="in-stock" />
-            <Label htmlFor="in-stock">In stock</Label>
-          </div>
-        </div>
-        <div className="space-y-3 ">
-          <h1 className="text-2xl uppercase">Price Range</h1>
-          <div className="max-w-xl">
-            <Slider
-              defaultValue={[150]}
-              max={150}
-              min={0}
-              step={1}
-              onValueChange={(value) => handleSlider(value)}
-            />
-          </div>
-          <div>From 0$ To {priceRange}$</div>
-        </div>
-      </div>
-      <div className="col-span-9 grid grid-cols-3 gap-10 pb-20">
+    <div className="grid grid-cols-12 max-w-7xl mx-auto relative">
+      <div className="col-span-12 grid grid-cols-3 gap-10 pb-20">
         {booksData?.map((book:IBook) => (
           <BookCard book={book} />
         ))}
